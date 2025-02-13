dec969632b8aa06d9681aa730c0aeccb4716169f
Sandip229 <sandipp2299@gmail.com>
2025-02-13 11:53:02 +0530

    Update sandip.sh

--- Scripts/sandip.sh
+++ Scripts/sandip.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-sudo apt-get update -y
+sudo apt update -y
 sudo apt-get install docker.io -y
 sudo systemctl start docker
-# sudo systemctl enable docker
-
 
 sudo docker build -t img1 /home/ubuntu/app/
-
 
 sudo docker run -d --name con-1 -p 80:80 img1 
